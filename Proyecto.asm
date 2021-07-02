51a0151299c611b410003515cfa1ad2c05cbedbb
Jorge Zambrano <jaaronzambranoch@outlook.es>
2021-07-02 10:36:13 -0500

    Archivo Inicial

---
+++ Proyecto.asm
@@ -0,0 +1,10 @@
+.model small
+.data
+    LF equ 10 ; salto de linea
+    CR equ 13 ; Retorno de carro
+    var1 db ?
+    msg1 db "-----------** Sopa de letras **----------- $",0
+    msg2    db "Escriba la palabra que encuentre, o escriba exit para salir$",0
+.code
+.start up  
+
