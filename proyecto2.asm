d9fedf12c7176fa2a79b457745d1023ce846d4eb
Jorge Zambrano <jaaronzambranoch@outlook.es>
2021-07-12 02:06:23 -0500

    variables en .data

--- proyecto2.asm
+++ proyecto2.asm
@@ -41,19 +41,188 @@ endm
     LF equ 10 ; salto de linea
     CR equ 13 ; Retorno de carro
     TB equ 09 ; tab
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Opciones - Menu ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
     var1 db ?
     menu1 db "-----------** Sopa de letras **-----------"  ,LF,CR,LF,TB
-         db  "Elija una tematica para su sopa de letra "  ,LF,CR,TB 
-         db   "1.- Animales "                            ,LF,CR,TB 
-         db   "2.- Vehiculos de transporte "              ,LF,CR,TB
-         db   "3.- Lenguajes de programacion "            ,LF,CR,,TB,LF
-         db   "Escriba el numero de la opcion que desea: $",LF,CR 
+          db "Elija una tematica para su sopa de letra "  ,LF,CR,TB 
+          db "1.- Animales "                            ,LF,CR,TB 
+          db "2.- Vehiculos de transporte "              ,LF,CR,TB
+          db "3.- Lenguajes de programacion "            ,LF,CR,,TB,LF
+          db "Escriba el numero de la opcion que desea: $",LF,CR 
+    
     op db 0
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Pantalla - Juego ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
     mOp1 db "Sopa de letras: Animales $"
     mOp2 db "Sopa de letras: Vehiculos de transporte $"
     mOp3 db "Sopa de letras: Lenguajes de programacion $"   
     volver db 0 
     msjError db CR,LF,TB, "Ingrese una opcion correcta: $"
+    ingreso db LF,"Ingreso: $"
+    cadena db 15,?,15 dup(' ')
+    temp db 15 dup(' ')
+    coordenadaI dw ?
+    coordenadaF dw ?
+    aciertos db 0
+    esExit db 0
+    mExit db "exit"
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   Sopa - Animales ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Opcion1;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    perro db "perro"
+    delfin db "delfin"
+    gato db "gato"    
+    tiburon db "tiburon"
+    leon db "leon"
+    
+    sopaA1 db "l a p e r r o c f t q c f t q",LF,CR
+           db "r a t q u x u r a t q u t o r",LF,CR
+           db "b r w v t i g e d e l f i n o",LF,CR
+           db "v r e y e b r b p a m b w x q",LF,CR 
+           db "l g d t d t f d i v v z v v c",LF,CR
+           db "e r c e c i w s y n f v x d x",LF,CR
+           db "o e z d b b e a r v m v x g z",LF,CR
+           db "n b d w b u q s u j e v x v q",LF,CR
+           db "b f t y c r p f z g a t o m d",LF,CR
+           db "n h u i n o m e g r m v f b v",LF,CR
+           db "j k c u x n v t h w j v v c u",LF,CR
+           db "g m j t o u w u z d x c y b t",LF,CR
+           db "o x p r t q y h x b v b f v y",LF,CR
+           db "t f o d w c b q v n o h e d m",LF,CR
+           db "a d w a n x n b a c m v w q r",LF,CR,"$"
+          
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Opcion2;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    caballo db "caballo"
+    llama db "llama"
+    avestruz db "avestruz"    
+    buey db "buey"
+    jirafa db "jirafa"
+    
+    sopaA2 db "l a f s f e a c f t q c f t l",LF,CR
+           db "r a t q u x u r a t q u t o l",LF,CR
+           db "b r w v t i g e z x l n p n a",LF,CR
+           db "v r a v e s t r u z m b w x m",LF,CR 
+           db "l g d t d t f d i v v z v v a",LF,CR
+           db "d r c e c c w s y n f v x d x",LF,CR
+           db "o e z d b b e a r v m v x g z",LF,CR
+           db "r b c a b a l l o j e v x v q",LF,CR
+           db "b f t y c k p f z i w t o m d",LF,CR
+           db "n h u i n o m e g r m v f b v",LF,CR
+           db "j k c u x n v t h a j v v c u",LF,CR
+           db "g m j t o u w u z f x c y b t",LF,CR
+           db "o x p r t q y h x a v b f v y",LF,CR
+           db "t b u e y c b q v n o h e d m",LF,CR
+           db "a d w a n x n b a c m v w q r",LF,CR,"$"
+          
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   Sopa - Transporte ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Opcion1;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    barco db "barco"
+    carro db "carro"
+    moto db "moto"    
+    bicicleta db "bicicleta"
+    avion db "avion"
+    
+    sopaT1 db "c s g a s f e c f t q c f t q",LF,CR
+           db "a a t q u x u r b a r c o o r",LF,CR
+           db "r r w v b a r c d e s c z x o",LF,CR
+           db "r r e m o t o b p a m b w x q",LF,CR 
+           db "o g d t d t f d i v v z v v c",LF,CR
+           db "e r c e c c w s y b f v x d x",LF,CR
+           db "b e z d b p e a r i m v x g z",LF,CR
+           db "n b d w b u q s u c e v x v q",LF,CR
+           db "b f t y c r p f z i p t t m d",LF,CR
+           db "n h u i n y m e g c m v f b v",LF,CR
+           db "j k c u a v i o n l j v v c u",LF,CR
+           db "g m j t o u w u z e x c y b t",LF,CR
+           db "o x p r t q y h x t v b f v y",LF,CR
+           db "t f o d w c b q v a o h e d m",LF,CR
+           db "a d w a n x n b a c m v w q r",LF,CR,"$"
+          
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Opcion2;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    tren db "tren"
+    yate db "yate"
+    submarino db "submarino"    
+    ferry db "ferry"
+    canoa db "canoa"
+    
+    sopaT2 db "l a f s f e a c f t q c f t l",LF,CR
+           db "r a f q u x u a a t q u t o l",LF,CR
+           db "b r e v t i g n z x l n p n a",LF,CR
+           db "v r r f s e f o f g m b w x m",LF,CR 
+           db "l g r t d t f a i v v z v v a",LF,CR
+           db "d r y e c c w s y n f v x d x",LF,CR
+           db "o e z d b b e a r v m v x g z",LF,CR
+           db "r b c a b a s u b m a r i n o",LF,CR
+           db "b f t y c k p f z i w t o m d",LF,CR
+           db "n h u i n o m e g r m v f b v",LF,CR
+           db "j k c u x n v t h t r e n c u",LF,CR
+           db "g m j t o u w u z f x c y b t",LF,CR
+           db "o x p r t q y h x a v b f v y",LF,CR
+           db "t y a t e c b q v n o h e d m",LF,CR
+           db "a d w a n x n b a c m v w q r",LF,CR,"$"
+           
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   Sopa - Programacion ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Opcion1;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    java db "java"
+    ruby db "ruby"
+    python db "python"    
+    swift db "swift"
+    dart db "dart"
+    
+    sopaP1 db "c s g a s f e c f t q c f t q",LF,CR
+           db "q a t q u x u r x a a c b o r",LF,CR
+           db "e r w v b d r c d e s c z x o",LF,CR
+           db "r r e m j a v a p a m b w x q",LF,CR 
+           db "o g d t d r f d i v v z v v c",LF,CR
+           db "e r c e c t w s y b f v x d x",LF,CR
+           db "b e r u b y e a r r m v x g z",LF,CR
+           db "n b d w b u q s u t e v x v q",LF,CR
+           db "b f t y c r p f z i p t t m d",LF,CR
+           db "n p u i n y m e g c m v f b v",LF,CR
+           db "j y c u a v l f s w i f t c u",LF,CR
+           db "g t j t o u w u z l x c y b t",LF,CR
+           db "o h p r t q y h x t v b f v y",LF,CR
+           db "t o o d w c b q v a o h e d m",LF,CR
+           db "a n w a n x n b a c m v w q r",LF,CR,"$"
+          
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Opcion2;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ;Java y Python se repiten
+    php db "php"    
+    javascript db "javascript"
+    kotlin db "kotlin"
+    
+    sopaP2 db "l a f s f p y t h o n c f t l",LF,CR
+           db "r a p q u x u a a t q u k o t",LF,CR
+           db "b r h v t i g n z x l n o n a",LF,CR
+           db "v r p f s e f o f g m b t x m",LF,CR 
+           db "l g r t d t f a i v v z l v a",LF,CR
+           db "d r y e c c w j y n f v i d x",LF,CR
+           db "o e z d b b e a r v m v n g z",LF,CR
+           db "r b c a b a s v b m a p i n o",LF,CR
+           db "b f t y c k p a z i w t o m d",LF,CR
+           db "n h u i n o m s g r m v f b v",LF,CR
+           db "j k c u x n v c h t r e n c u",LF,CR
+           db "g m j t o u w r z f x c y b t",LF,CR
+           db "o x p r t q y i x j a v a v y",LF,CR
+           db "t y p s e c b p v n o h e d m",LF,CR
+           db "a d w a n x n t a c m v w q r",LF,CR,"$"
+               
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    
     
 .code
 .start up    
